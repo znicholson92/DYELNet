ae930180673a17cc6a580413f21c768ddebee78e
gfpastorek <gfpastorek@gmail.com>
2013-10-16 20:18:33 -0500

    Commit, does not function
    Click connect to connect to the web and read the JSON returned and write
    it to a listView.  There is an error somewhere in reading the JSON and
    writing it to the listView

--- src/com/example/dyel_net/MainActivity.java
+++ src/com/example/dyel_net/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.dyel_net;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.app.*;
@@ -40,7 +41,6 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		listView1 = (ListView) findViewById(R.id.listView1);
-		accessWebService();
 	}
 
 	@Override
@@ -51,11 +51,13 @@ public class MainActivity extends Activity {
 	}
 	
 	
-	public void connectToDatabase()
+	public void connectToDatabase(View v)
 	{
 		AlertDialog dialog = new AlertDialog.Builder(this).create();
 	    dialog.setTitle("Title");
 	    dialog.show();
+	    accessWebService();
+	    //ListDrawer();
 	}
 	
 	private class JsonReadTask extends AsyncTask<String, Void, String> {
@@ -89,7 +91,7 @@ public class MainActivity extends Activity {
 		   }
 		 
 		   catch (IOException e) {
-		    // e.printStackTrace();
+		     e.printStackTrace();
 		    Toast.makeText(getApplicationContext(),
 		      "Error..." + e.toString(), Toast.LENGTH_LONG).show();
 		   }
@@ -111,7 +113,7 @@ public class MainActivity extends Activity {
 		 // build hash set for list view
 		 public void ListDrawer() {
 		  List<Map<String, String>> resultList = new ArrayList<Map<String, String>>();
-		 //{"data":[{"username":"testuser","firstname":"Stiff","lastname":"Rick","dateofbirth":"1990-01-01","sex":"m","location":null}]} 
+		 
 		  try {
 		   JSONObject jsonResponse = new JSONObject(jsonResult);
 		   JSONArray jsonMainNode = jsonResponse.optJSONArray("data");
@@ -149,7 +151,100 @@ public class MainActivity extends Activity {
 		  resultMap.put(name, data);
 		  return resultMap;
 		 }
-	
-	
+		 
+		 
+		 /*
+		 class LoadAllIcons extends AsyncTask<String, String, String> {
+
+		
+			    @Override
+			    protected void onPreExecute() {
+			        super.onPreExecute();
+			        pDialog = new ProgressDialog(AllIconsActivity.this);
+			        pDialog.setMessage("Loading icons. Please wait...");
+			        pDialog.setIndeterminate(false);
+			        pDialog.setCancelable(false);
+			        pDialog.show();
+			    }
+
+			    
+			    // getting All products from url
+			     
+			    protected String doInBackground(String... args) {
+			        // Building Parameters
+			        List<NameValuePair> params = new ArrayList<NameValuePair>();
+			        // getting JSON string from URL
+			        JSONObject json = jParser.makeHttpRequest(url_all_icons, "GET", params);
+
+			        // Check your log cat for JSON reponse
+			        Log.d("All Icons: ", json.toString());
+
+			        try {
+			            // Checking for SUCCESS TAG
+			            int success = json.getInt(TAG_SUCCESS);
+
+			            if (success == 1) {
+			                // products found
+			                // Getting Array of Products
+			                TB_MainContent = json.getJSONArray(TAG_TB_MainContent);
+
+			                // looping through All Products
+			                for (int i = 0; i < TB_MainContent.length(); i++) {
+			                    JSONObject c = TB_MainContent.getJSONObject(i);
+
+			                    // Storing each json item in variable
+			                    String id = c.getString(TAG_ID);
+			                    String iconname = c.getString(TAG_ICONNAME);
+
+			                    // creating new HashMap
+			                    HashMap<String, String> map = new HashMap<String, String>();
+
+			                    // adding each child node to HashMap key => value
+			                    map.put(TAG_ID, id);
+			                    map.put(TAG_ICONNAME, iconname);
+
+			                    // adding HashList to ArrayList
+			                    iconsList.add(map);
+			                }
+			            } else {
+			                // no products found
+			                // Launch Add New product Activity
+			                Intent i = new Intent(getApplicationContext(),
+			                        NewIconActivity.class);
+			                // Closing all previous activities
+			                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			                startActivity(i);
+			            }
+			        } catch (JSONException e) {
+			            e.printStackTrace();
+			        }
+
+			        return null;
+			    }
+
+			    
+			    //After completing background task Dismiss the progress dialog
+			    
+			    protected void onPostExecute(String file_url) {
+			        // dismiss the dialog after getting all products
+			        pDialog.dismiss();
+			        // updating UI from Background Thread
+			        runOnUiThread(new Runnable() {
+			            public void run() {
+			                
+			                 //Updating parsed JSON data into ListView
+			                 
+			                 adapter = new SimpleAdapter(
+			                        AllIconsActivity.this, iconsList,
+			                        R.layout.screen_list, new String[] { TAG_ID,
+			                                TAG_ICONNAME},
+			                        new int[] { R.id.id, R.id.iconname });
+			                // updating listview
+			                gridView.setAdapter(adapter);
+			            }
+			        });
+
+			    }
+		 }*/
 
 }
